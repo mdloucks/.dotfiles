544ec1c30a7f5b3d3b0615657b8f7a7ec8141119
Matt Loucks <mloucks@whisker.com>
2024-08-02 12:09:24 -0400

    change dropdown

--- .config/nvim/lua/plugins/configs/telescope.lua
+++ .config/nvim/lua/plugins/configs/telescope.lua
@@ -1,23 +1,10 @@
 local options = {
-
-  pickers = {
-    find_files = {
-      theme = 'dropdown',
-    },
-  },
   defaults = {
+    file_ignore_patterns = {},
     initial_mode = 'insert',
     selection_strategy = 'reset',
     sorting_strategy = 'ascending',
     layout_strategy = 'horizontal',
-    layout_config = {
-      horizontal = {
-        prompt_position = 'top',
-      },
-      vertical = {
-        mirror = false,
-      },
-    },
   },
 }
 
