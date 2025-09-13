f2bb7ce9ae672c1dadd48f3c19ed9e9b857ae8cf
Matt Loucks <mloucks@whisker.com>
2025-09-13 15:37:56 -0400

    feat: closing tags + fvm

--- .config/nvim/lua/plugins/flutter-tools.lua
+++ .config/nvim/lua/plugins/flutter-tools.lua
@@ -8,9 +8,4 @@ return {
   --   exception_breakpoints = {},
   --   run_via_dap = true,
   -- },
-  closing_tags = {
-    highlight = "ErrorMsg", -- highlight for the closing tag
-    prefix = ">",           -- character to use for close tag e.g. > Widget
-    enabled = true          -- set to false to disable
-  },
 }
